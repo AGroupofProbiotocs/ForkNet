b688fb2f4cefbf9511bcb8b75f1c72e1343e3d54
AGroupofProbiotocs <33567919+AGroupofProbiotocs@users.noreply.github.com>
2020-06-23 20:27:14 +0800

    Update test_on_real_image.py

--- test_on_real_image.py
+++ test_on_real_image.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+========================================================================
+ForkNet for DoFP sensor to reconstruct s0, dolp and aop, Version 1.0
+Copyright(c) 2020 Xianglong Zeng, Yuan Luo, Xiaojing Zhao, Wenbin Ye
+All Rights Reserved.
+----------------------------------------------------------------------
+Permission to use, copy, or modify this software and its documentation
+for educational and research purposes only and without fee is here
+granted, provided that this copyright notice and the original authors'
+names appear on all copies and supporting documentation. This program
+shall not be used, rewritten, or adapted as the basis of a commercial
+software or hardware product without first obtaining permission of the
+authors. The authors make no representations about the suitability of
+this software for any purpose. It is provided "as is" without express
+or implied warranty.
+----------------------------------------------------------------------
+Please cite the following paper when you use it:
+
+Xianglong Zeng, Yuan Luo, Xiaojing Zhao, and Wenbin Ye, "An end-to-end 
+fully-convolutional neural network for division of focal plane sensors 
+to reconstruct S0, DoLP, and AoP," Opt. Express 27, 8566-8577 (2019)
+========================================================================
+"""
 import tensorflow as tf
 import numpy as np
 import cv2
