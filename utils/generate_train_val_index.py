c8d55c742d721c0839b584e37b53a5ee57f02d63
AGroupofProbiotocs <33567919+AGroupofProbiotocs@users.noreply.github.com>
2020-06-25 10:54:02 +0800

    Add files via upload

---
+++ utils/generate_train_val_index.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 22 21:07:21 2018
+
+@author: Dragon
+"""
+import numpy as np
+
+img_num = 110
+
+#随机打乱图片序号并划分训练及验证集
+# img_index = list(range(img_num))
+# np.random.shuffle(img_index)
+# img_index = str(img_index)[1:-1]
+img_index = list(np.random.permutation(img_num))
+train_img_index = str(img_index[:100])[1:-1]
+val_img_index = str(img_index[100:])[1:-1]
+
+with open('../list/train_image_index.list', 'w') as f:
+    f.write(train_img_index)
+
+with open('../list/val_image_index.list', 'w') as f:
+    f.write(val_img_index)
